9edaf8ffa2daa651d48dcacd746127fc14c38f9d
nhanna4605 <doanhnhan4605@gmail.com>
2026-04-16 00:01:09 +0700

    Delete booksdb.js

--- booksdb.js
+++
@@ -1,6 +0,0 @@
-let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} }
-      // Thêm các cuốn khác nếu muốn...
-}
-module.exports=books;
